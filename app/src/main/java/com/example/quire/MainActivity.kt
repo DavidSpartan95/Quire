bc7771eddb1a20a54a2d9660e2f2d43f950fda5a
livbogdan <33436357+livbogdan@users.noreply.github.com>
2023-05-05 12:21:58 +0200

    test

--- app/src/main/java/com/example/quire/MainActivity.kt
+++ app/src/main/java/com/example/quire/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    //GG
                     Greeting("Quire")
                 }
             }
